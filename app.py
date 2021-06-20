1421e4b14c0a4682a55d7f4a7d6aefd9a33c1d61
sairaj1234 <81349292+sairaj1234@users.noreply.github.com>
2021-06-20 22:01:05 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask,render_template,request
+import numpy as np
+import pickle
+app=Flask(__name__)
+model=pickle.load(open('model.pkl','rb'))
+@app.route('/')
+def welcome():
+    return render_template('index.html')
+@app.route('/submit',methods=['POST'])
+def predt():
+    
+    fea=float(request.form['salary'])
+    pred=[[fea]]
+    predicition=model.predict(pred)[0][0]
+    return render_template('index.html',res='salary is {:.2f}'.format(predicition))
+
+
+    
+if __name__=='__main__':
+    app.run(debug=True)
